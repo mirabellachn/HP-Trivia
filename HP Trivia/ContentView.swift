bae7ff723ee872dbc11932988602e91999fedc32
mirabellachn <mirabellacn@gmail.com>
2025-02-20 02:36:08 +0700

    gameplay views

--- HP Trivia/ContentView.swift
+++ HP Trivia/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
     @State private var animateViewsIn = false
     @State private var showInstructions = false
     @State private var showSettings = false
+    @State private var playGame = false
     
     var body: some View {
         GeometryReader { geo in
@@ -101,7 +102,7 @@ struct ContentView: View {
                         VStack {
                             if animateViewsIn {
                                 Button{
-                                    // Start a new game
+                                    playGame.toggle()
                                 } label: {
                                     Text("Play")
                                         .font(.largeTitle)
@@ -120,6 +121,9 @@ struct ContentView: View {
                                 }
                                 
                                 .transition(.offset(y: geo.size.height/3))
+                                .fullScreenCover(isPresented: $playGame) {
+                                    Gameplay()
+                                }
                             }
                         }
                         .animation(.easeOut(duration: 0.7).delay(2), value: animateViewsIn)
