b790f7a84cb4661758a3735b9833638ffb1bd7e7
mirabellachn <mirabellacn@gmail.com>
2025-02-19 17:06:40 +0700

    tap gestures

--- HP Trivia/ContentView.swift
+++ HP Trivia/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
     @State private var moveBackgroundImage = false
     @State private var animateViewsIn = false
     @State private var showInstructions = false
+    @State private var showSettings = false
     
     var body: some View {
         GeometryReader { geo in
@@ -129,7 +130,7 @@ struct ContentView: View {
                         VStack {
                             if animateViewsIn {
                                 Button {
-                                    
+                                    showSettings.toggle()
                                 } label: {
                                     Image(systemName: "gearshape.fill")
                                         .font(.largeTitle)
@@ -139,6 +140,9 @@ struct ContentView: View {
                                 }
                                 
                                 .transition(.offset(x: -geo.size.width/4))
+                                .sheet(isPresented: $showSettings) {
+                                    Settings()
+                                }
                             }
                         }
                         .animation(.easeOut(duration: 0.7).delay(2), value: animateViewsIn)
