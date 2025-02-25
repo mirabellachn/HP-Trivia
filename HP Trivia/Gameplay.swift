2cf70107e3d87afac1a69d5e1aac7a51f1767a2a
mirabellachn <mirabellacn@gmail.com>
2025-02-25 22:05:15 +0700

    celebration gameplay

--- HP Trivia/Gameplay.swift
+++ HP Trivia/Gameplay.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct Gameplay: View {
     @State private var animateViewsIn = false
+    @State private var tappedCorrectAnswer = false
     
     var body: some View {
         GeometryReader { geo in
@@ -111,12 +112,75 @@ struct Gameplay: View {
                 }
                 .frame(width: geo.size.width, height: geo.size.height)
                 .foregroundColor(.white)
+                
+                
+                // MARK: Celebration
+                VStack {
+                    Spacer()
+                    
+                    VStack {
+                        if tappedCorrectAnswer {
+                            Text("5")
+                                .font(.largeTitle)
+                                .padding(.top, 50)
+                                .transition(.offset(y: -geo.size.height/4))
+                        }
+                    }
+                    .animation(.easeInOut(duration: 1).delay(2), value: tappedCorrectAnswer)
+                    
+                    Spacer()
+                    
+                    
+                    VStack {
+                        if tappedCorrectAnswer {
+                            Text("Brilliant!")
+                                .font(.custom(Constants.hpFont, size: 100))
+                                .transition(.scale.combined(with: .offset(y: -geo.size.height/2)))
+                        }
+                    }
+                    .animation(.easeInOut(duration: 1).delay(1), value: tappedCorrectAnswer)
+                    
+                    Spacer()
+                    
+                    Text("Answer 1")
+                        .minimumScaleFactor(0.5)
+                        .multilineTextAlignment(.center)
+                        .frame(width: geo.size.width/2.15, height: 80)
+                        .background(.green.opacity(0.5))
+                        .cornerRadius(25)
+                        .scaleEffect(2)
+                    
+                    Group {
+                        Spacer()
+                        Spacer()
+                    }
+                    
+                    VStack {
+                        if tappedCorrectAnswer {
+                            Button("Next Level>") {
+                                // TODO:  Reset level for next question
+                            }
+                            .buttonStyle(.borderedProminent)
+                            .tint(.blue.opacity(0.5))
+                            .font(.largeTitle)
+                            .transition(.offset(y: geo.size.height/3))
+                        }
+                    }
+                    .animation(.easeInOut(duration: 2.7).delay(2.7), value: tappedCorrectAnswer)
+                    
+                        Group {
+                            Spacer()
+                            Spacer()
+                        }
+                }
+                .foregroundColor(.white)
             }
             .frame(width: geo.size.width, height: geo.size.height)
         }
         .ignoresSafeArea()
         .onAppear() {
-            animateViewsIn = true
+//            animateViewsIn = true
+            tappedCorrectAnswer = true
         }
     }
 }
